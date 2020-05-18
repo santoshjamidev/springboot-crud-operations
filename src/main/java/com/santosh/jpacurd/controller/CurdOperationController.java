b9fd0cbcc9d2575cd550acf1158607037557f6d7
Santosh Jami <santoshjami@Santoshs-MacBook-Pro.local>
2020-05-17 22:06:12 -0400

    Base project with hello messsage

---
+++ src/main/java/com/santosh/jpacurd/controller/CurdOperationController.java
@@ -0,0 +1,21 @@
+package com.santosh.jpacurd.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/curd")
+public class CurdOperationController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CurdOperationController.class);
+
+    @GetMapping(path = "/insert")
+    public void insertEntryIntoTable(@RequestParam("empName") String empName) {
+
+        LOGGER.info("CurdOperationController: Emp Name is "+empName);
+    }
+}
