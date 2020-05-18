76558d224df261180e874164bdaca9d0f9e2bda4
Santosh Jami <santoshjami@Santoshs-MacBook-Pro.local>
2020-05-17 20:06:14 -0400

    initial project setup

---
+++ src/main/java/com/santosh/jpacurd/JpacurdApplication.java
@@ -0,0 +1,13 @@
+package com.santosh.jpacurd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpacurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpacurdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santosh/jpacurd/JpacurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosh.jpacurd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpacurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
